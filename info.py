a25fd296b86818451fa56b0c15cca8c3dc96ee31
umangm0389-sketch <umangm0389@gmail.com>
2025-12-28 18:26:38 +0530

    Change default API_ID in info.py
    Update default API_ID to '73839' in configuration.

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
+API_ID = int(environ.get('API_ID', '73839')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
 BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
 
